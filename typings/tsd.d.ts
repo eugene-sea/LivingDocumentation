693da07f74fb46b770799e30c58faca4c14c1f30
Eugene Shalyuk <Eugene.SEA@gmail.com>
2015-09-19 14:43:20 +0300

    Updated components

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -10,3 +10,5 @@
 /// <reference path="express/express.d.ts" />
 /// <reference path="should/should.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
