1c42b5d6c3f4cd833568ffd03bc676a092f6ffeb
Eugene Shalyuk <Eugene.SEA@gmail.com>
2016-04-02 19:12:09 +0300

    Minor change in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ tsc -p ./test
 
 mkdir -p ./app/node_modules/systemjs
 cp ./node_modules/systemjs/dist/system.js ./app/node_modules/systemjs/system.js
+cp ./node_modules/systemjs/dist/system.js.map ./app/node_modules/systemjs/system.js.map
 
 mkdir -p ./app/node_modules/typescript
 cp ./node_modules/typescript/lib/typescript.js ./app/node_modules/typescript/typescript.js
