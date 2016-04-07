829a038fb41d92dd0989f7c85061c67546520c1d
Eugene Shalyuk <Eugene.SEA@gmail.com>
2016-04-07 11:03:39 +0300

    Refactoring

--- app/components/directives.ts
+++ app/components/directives.ts
@@ -14,46 +14,6 @@ class AppVersion implements ng.IDirective {
     }
 }
 
-class IsActive implements ng.IDirective {
-    public static $inject: string[] = ['$location'];
-
-    constructor(private $location: ng.ILocationService) {
-        this.link = (scope, element, attributes) => this.linkCore(scope, element, attributes);
-    }
-
-    public link: (scope: ng.IScope, element: JQuery, attributes: any) => any;
-
-
-    private static subscribe(scope: ng.IScope, handler: () => void): void {
-        scope.$on('$routeChangeSuccess', handler);
-        scope.$on('$includeContentLoaded', handler);
-    }
-
-    private linkCore(scope: ng.IScope, element: JQuery, attributes: any): any {
-        let handler = () => {
-            let isActive: boolean;
-            if (attributes['isActive']) {
-                isActive = this.$location.path().indexOf(attributes['isActive']) === 0;
-            } else {
-                let indexOf = this.$location.path().indexOf(attributes['isActiveLast']);
-                isActive = indexOf >= 0 &&
-                    (indexOf + attributes['isActiveLast'].length === this.$location.path().length);
-            }
-
-            if (isActive) {
-                element.addClass('active');
-            } else {
-                element.removeClass('active');
-            }
-        };
-
-        handler();
-        IsActive.subscribe(scope, handler);
-    }
-}
-
 angular
     .module('livingDocumentation.directives', [])
-    .directive('appVersion', wrapInjectionConstructor(AppVersion))
-    .directive('isActive', wrapInjectionConstructor(IsActive))
-    .directive('isActiveLast', wrapInjectionConstructor(IsActive));
+    .directive('appVersion', wrapInjectionConstructor(AppVersion));
